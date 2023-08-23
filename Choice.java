5f1c8c0838fa44dac1926d7ac68124fa012043d4
teixeiralucs <tlucasmatheus@gmail.com>
2023-08-22 22:07:09 -0300

    added gets and sets on choice class

--- Choice.java
+++ Choice.java
@@ -1,6 +1,6 @@
 public class Choice {
-    String answer;
-    Chapter next;
+    private String answer;
+    private Chapter next;
 
     public Choice( String answer, Chapter next) {
         this.answer = answer;
@@ -11,5 +11,17 @@ public String getanswer() {
 }
 public Chapter getnext() {
     return next;
+}
+public String getAnswer() {
+    return answer;
+}
+public void setAnswer(String answer) {
+    this.answer = answer;
+}
+public Chapter getNext() {
+    return next;
+}
+public void setNext(Chapter next) {
+    this.next = next;
 }
 }
